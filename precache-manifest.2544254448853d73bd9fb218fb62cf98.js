7004c0794dbeedd5f32a434c051a48f02ad6232d
David <eliuxj@163.com>
2021-08-23 17:52:34 +0800

    Initial commit

---
+++ precache-manifest.2544254448853d73bd9fb218fb62cf98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfa2d8552d4e20ef82b181aee288a63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63150930678557dd434c",
+    "url": "./static/css/main.7f77a147.chunk.css"
+  },
+  {
+    "revision": "b5d8ce8cda8d881ca4c5",
+    "url": "./static/js/2.aa84e60a.chunk.js"
+  },
+  {
+    "revision": "cfaee8b6dfac10b370cbe825fd489b14",
+    "url": "./static/js/2.aa84e60a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63150930678557dd434c",
+    "url": "./static/js/main.bd29853a.chunk.js"
+  },
+  {
+    "revision": "d622694fca4ff4487828",
+    "url": "./static/js/runtime-main.68000414.js"
+  },
+  {
+    "revision": "60ddac09eb9b2820d2d4f774cc20a3bf",
+    "url": "./static/media/logo.60ddac09.jpg"
+  }
+]);
